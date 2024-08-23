5b53521ed7d2e60f4e2d43d8b82456e9545f469c
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-23 19:18:29 -0400

    Created the Person and PersonAddRequest classes

---
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceContracts.Enums;
+
+namespace ServiceContracts.DTO
+{
+    public class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+    }
+}
