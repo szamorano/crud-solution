dc5748bd57bc46eba8d33c06843ef9b55d15f9df
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-28 18:17:24 -0400

    Add and use entity framework core

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -1,11 +1,15 @@
 using ServiceContracts;
 using Services;
+using Microsoft.EntityFrameworkCore;
+using Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddSingleton<ICountriesService, CountriesService>();
 builder.Services.AddSingleton<IPersonsService, PersonsService>();
+
+builder.Services.AddDbContext<PersonsDbContext>(options => { options.UseSqlServer(); });
 
 var app = builder.Build();
 
