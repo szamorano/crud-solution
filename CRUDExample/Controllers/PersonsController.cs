924ef2f67248f369c254c5b2c698a942a46a4c83
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-30 19:03:18 -0400

    Created a downloadable pdf link, view, and method of users listed.

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Rotativa.AspNetCore;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
@@ -146,6 +147,21 @@ namespace CRUDExample.Controllers
 
             await _personsService.DeletePerson(personDeleteRequest.PersonID);
             return RedirectToAction("Index");
+        }
+
+        [Route("PersonsPDF")]
+        public async Task<IActionResult> PersonsPDF()
+        {
+            List<PersonResponse> persons = await _personsService.GetAllPersons();
+
+            return new ViewAsPdf("PersonsPDF", persons, ViewData)
+            {
+                PageMargins = new Rotativa.AspNetCore.Options.Margins()
+                {
+                    Top = 20, Right = 20, Bottom = 20, Left = 20 
+                },
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Landscape,
+            };
         }
     }
 }
--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -23,6 +23,7 @@ if(builder.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
