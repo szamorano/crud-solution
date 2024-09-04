ac865cfe23bec11971a5fb7efc19fcc7884bbc9e
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-09-04 18:02:50 -0400

    Added the PersonsListActionFilter, and added some regular logs, and some structured logs.

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDExample.Filters.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Rotativa.AspNetCore;
 using ServiceContracts;
@@ -23,6 +24,7 @@ namespace CRUDExample.Controllers
 
         [Route("[action]")]
         [Route("/")]
+        [TypeFilter(typeof(PersonsListActionFilter))]
         public async Task<IActionResult> Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.Ascending)
         {
             _logger.LogInformation("Index action method of PersonsController");
---
+++ CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,93 @@
+﻿using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
+
+namespace CRUDExample.Filters.ActionFilters
+{
+    public class PersonsListActionFilter : IActionFilter
+    {
+        private readonly ILogger<PersonsListActionFilter> _logger;
+
+        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //_logger.LogInformation("PersonsListActionFilter.OnActionExecuted method");
+
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuted));
+
+
+
+
+
+
+            //PersonsController personsController = (PersonsController)context.Controller;
+
+            //IDictionary<string, object?>? parameters = (IDictionary<string, object?>?)context.HttpContext.Items["arguments"];
+
+            //if (parameters != null) 
+            //{ 
+            //    if(parameters.ContainsKey("searchBy"))
+            //    {
+            //        personsController.ViewData["CurrentSearchBy"] = Convert.ToString(parameters["searchBy"]);
+            //    }
+
+            //    if (parameters.ContainsKey("searchString"))
+            //    {
+            //        personsController.ViewData["CurrentSearchString"] = Convert.ToString(parameters["searchString"]);
+            //    }
+
+            //    if (parameters.ContainsKey("sortBy"))
+            //    {
+            //        personsController.ViewData["CurrentSortBy"] = Convert.ToString(parameters["sortBy"]);
+            //    }
+
+            //    if (parameters.ContainsKey("sortOrder"))
+            //    {
+            //        personsController.ViewData["CurrentSortOrder"] = Convert.ToString(parameters["sortOrder"]);
+            //    }
+            //}
+        }
+
+
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            context.HttpContext.Items["arguments"] = context.ActionArguments;
+
+            _logger.LogInformation("PersonsListActionFilter.OnActionExecuting method");
+            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(PersonsListActionFilter), nameof(OnActionExecuting));
+
+            if (context.ActionArguments.ContainsKey("searchBy"))
+            {
+                string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+
+                if (!string.IsNullOrEmpty(searchBy))
+                {
+                    var searchByOptions = new List<string>()
+                    {
+                        nameof(PersonResponse.PersonName),
+                        nameof(PersonResponse.Email),
+                        nameof(PersonResponse.DateOfBirth),
+                        nameof(PersonResponse.Gender),
+                        nameof(PersonResponse.CountryID),
+                        nameof(PersonResponse.Address),
+                    };
+
+                    if (searchByOptions.Any(temp => temp == searchBy) == false)
+                    {
+                        _logger.LogInformation("searchBy actual value {searchBy}", searchBy);
+                        context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+                        _logger.LogInformation("searchBy updated value {searchBy}", context.ActionArguments["searchBy"]);
+                    }
+                }
+
+
+            }
+        }
+    }
+}
